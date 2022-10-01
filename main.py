2dcdc555842a071419f174782a007e8ac4ab2e89
ankumar8164 <105609968+ankumar8164@users.noreply.github.com>
2022-10-01 12:01:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ import requests
 from googlesearch import search
 from nltk.corpus import wordnet
 # bot = commands.Bot(command_prefix='!', intents = discord.Intents.all())
-bot = interactions.Client(dotenv_values(".env")["TOKEN"])
+# bot = interactions.Client(dotenv_values(".env")["TOKEN"])
+bot = interactions.Client(os.envrion["TOKEN"])
 bot.rsoLink = "https://illinois.campuslabs.com/engage/organization/"
 # client = interactions.Client(os.getenv("TOKEN"))
 bot.linkDict = {}
